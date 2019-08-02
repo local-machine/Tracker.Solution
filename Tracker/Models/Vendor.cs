683fcb8eb42d88bfd10f2899a1946eb753d6c324
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-08-02 14:35:09 -0700

    Created Vendor object and ran tests on said object

--- Tracker/Models/Vendor.cs
+++ Tracker/Models/Vendor.cs
@@ -1,12 +1,36 @@
-// using System.Collections.Generic;
+using System.Collections.Generic;
 
-// namespace Tracker.Models
-// {
-//   public class Order
-//   {
-//     private static List<Order> _instances = new List<Order> {};
-//     public string Name { get; set; }
-//     public int Id { get; }
-//     public List<Vendor> Vendors { get; set; }
-//   }
-// }
+namespace Tracker.Models
+{
+  public class Vendor
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public List<Order> Orders { get; set; }
+    public Vendor(string vendorName, string vendorDescription)
+    {
+      Name = vendorName;
+      Description = vendorDescription;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order> { };
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
